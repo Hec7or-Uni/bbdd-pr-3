99bb33d245404730cd99aaf087cd40c7106d9c23
hec7orci7o <hectortoraltrabajos@gmail.com>
2022-05-19 12:13:36 +0200

    primer trigger

--- entregables/triggers.sql
+++ entregables/triggers.sql
@@ -1,4 +1,19 @@
 -- TRIGGER 1:
+CREATE OR REPLACE TRIGGER EXC_PELIS
+BEFORE INSERT ON desvios
+FOR EACH ROW
+DECLARE 
+    flag NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO flag
+    FROM cancelaciones
+    WHERE idVuelo = :NEW.idVuelo;
+
+    IF flag >= 1 THEN
+        RAISE_APPLICATION_ERROR (-20000, 'No puede haber desvio puesto que el vuelo ha sido cancelado');
+    END IF;
+END; 
+/
 
 -- TRIGGER 2:
 
