591c49a78a2c3040df49b291a6ec8d5ef058e950
hec7orci7o <hectortoraltrabajos@gmail.com>
2022-05-21 14:38:22 +0200

    segundo trigger?

--- entregables/triggers.sql
+++ entregables/triggers.sql
@@ -1,5 +1,5 @@
 -- TRIGGER 1:
-CREATE OR REPLACE TRIGGER EXC_PELIS
+CREATE OR REPLACE TRIGGER EXC_DESVIOS
 BEFORE INSERT ON desvios
 FOR EACH ROW
 DECLARE 
@@ -16,18 +16,24 @@ END;
 /
 
 -- TRIGGER 2:
-CREATE OR REPLACE TRIGGER EXC_PELIS
+CREATE OR REPLACE TRIGGER EXT_PRIMER_DESVIO
 AFTER INSERT ON desvios
 FOR EACH ROW
 DECLARE 
     flag NUMBER;
+    o_tailNum VARCHAR(7);
 BEGIN
-    SELECT COUNT(*) INTO flag
-    FROM desvios
-    WHERE idVuelo = :NEW.idVuelo;
+    SELECT tailNum INTO origin
+    FROM vuelo
+    WHERE id = :NEW.idVuelo;
 
-    IF flag < 1 THEN
-        RAISE_APPLICATION_ERROR (-20000, 'No existe el primer desvio del vuelo' || :NEW.idVuelo);
+    IF origin <> :NEW.origin THEN   -- No es el primer vuelo
+        SELECT COUNT(*) INTO flag
+        FROM desvios
+        WHERE tailNum = :NEW.tailNum;
+        IF flag < 1 THEN
+            RAISE_APPLICATION_ERROR (-20000, 'No existe el primer desvio del vuelo: ' || :NEW.idVuelo || ', con matricula: ' || o_tailNum);
+        END IF;
     END IF;
 END; 
 /
