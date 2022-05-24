61c92732e993263d02219181bf40f3744d568349
pablolftw <779739@unizar.es>
2022-05-24 15:12:51 +0200

    Update triggers.sql

--- entregables/triggers.sql
+++ entregables/triggers.sql
@@ -38,19 +38,18 @@ BEGIN
 END; 
 /
 
--- TRIGGER 3: NO puede haber retraso si hay cancelacion
-CREATE OR REPLACE TRIGGER EXC_RETRASOS
-BEFORE INSERT ON retrasos
+-- TRIGGER 3: Un desvio no puede tener un origen distinto al origen del vuelo original o un desvio no puede tener un destino igual al destino del vuelo original
+CREATE OR REPLACE TRIGGER DIST_DESVIO
+AFTER INSERT ON desvios
 FOR EACH ROW
 DECLARE 
     flag NUMBER;
 BEGIN
     SELECT COUNT(*) INTO flag
-    FROM cancelaciones
-    WHERE idVuelo = :NEW.idVuelo;
-
-    IF flag >= 1 THEN
-        RAISE_APPLICATION_ERROR (-20000, 'No puede haber retraso puesto que el vuelo ha sido cancelado');
+    FROM vuelo
+    WHERE id = :NEW.idVuelo AND (origin <> NEW.origin OR destination = NEW.destination)
+        IF flag < 1 THEN
+            RAISE_APPLICATION_ERROR (-20000, 'Error al añadir desvio: el origen del vuelo es distinto al origen del desvio o el destino del vuelo es 
+                                    igual al destino del desvio');
     END IF;
 END; 
-/
