4b15820f5627bb0f5bc2349afab30e1ed7c94d0c
hec7orci7o <hectortoraltrabajos@gmail.com>
2022-05-29 20:00:27 +0200

    consultas hechas

--- entregables/consultas.sql
+++ entregables/consultas.sql
@@ -18,60 +18,75 @@ from (
 -- Obtener el aeropuerto en el que operan los aviones más modernos (es decir, con menor media de edad). Obtener
 -- tanto el nombre y el código del aeropuerto como la media de edad de los aviones que operan en él.
 
--- nueva, falta sacar la primera linea
-select AE.airport, AE.iata, AVG(2022 - AV.year) as average
-from aeropuerto AE, vuelo V, avion AV
-where (AE.iata = V.origin or AE.iata = V.origin) and AV.tailNum = V.tailNum and
-	AV.year is not null
-group by AE.iata
-order by 3;
-
--- vieja
-SELECT B.iata, B.media, A.airport FROM aeropuerto A (
-    SELECT * FROM (
-        SELECT * FROM (
-            SELECT iata,AVG(2022-year) media FROM (
-                SELECT * FROM aeropuerto,vuelo,avion WHERE avion.tailNum = vuelo.tailNum 
-                AND (vuelo.destination = aeropuerto.iata OR vuelo.origin = aeropuerto.iata)
-            ) GROUP BY iata)
-        ORDER BY media
-    ) WHERE ROWNUM = 1
-) B WHERE A.iata = B.iata;
+select S.airport, S.iata, FLOOR(S.average)
+from (
+	select AE.airport, AE.iata, AVG(2022 - AV.year) as average
+	from aeropuerto AE, vuelo V, avion AV
+	where (AE.iata = V.origin or AE.iata = V.origin) and AV.tailNum = V.tailNum and
+		AV.year is not null
+	group by AE.iata
+) S
+where S.average = (
+	select MIN(average) as minimo
+	from (
+		select AE.airport, AE.iata, AVG(2022 - AV.year) as average
+		from aeropuerto AE, vuelo V, avion AV
+		where (AE.iata = V.origin or AE.iata = V.origin) and AV.tailNum = V.tailNum and
+			AV.year is not null
+		group by AE.iata
+	) A
+);
 
 -- CONSULTA 3
 -- Obtener la compañía (o compañías, en caso de empate) con el mayor porcentaje de vuelos que despegan y aterrizan
 -- en un mismo estado.
 
--- nueva, falta sacar la primera linea
-select A.carrier, round((A.num / B.num * 100), 2) as 'porcentaje'
-from (
-	select S.carrier, COUNT(*) as num
+select A.name
+from aerolinea A, (
+	select A.carrier, ROUND((A.num / B.num * 100), 2) as 'porcentaje'
 	from (
-		select V.carrier, A1.state as origin, A2.state as destination
-		from vuelo V, aeropuerto A1, aeropuerto A2
-		where V.origin = A1.iata and V.destination = A2.iata and A1.state = A2.state
-	) S
-	group by S.carrier
-) A, (
-	select T.carrier, COUNT(*) as num
-	from (
-		select V.carrier, A1.state as origin, A2.state as destination
-		from vuelo V, aeropuerto A1, aeropuerto A2
-		where V.origin = A1.iata and V.destination = A2.iata
-	) T
-	group by T.carrier
+		select S.carrier, COUNT(*) as num
+		from (
+			select V.carrier, A1.state as origin, A2.state as destination
+			from vuelo V, aeropuerto A1, aeropuerto A2
+			where V.origin = A1.iata and V.destination = A2.iata and A1.state = A2.state
+		) S
+		group by S.carrier
+	) A, (
+		select T.carrier, COUNT(*) as num
+		from (
+			select V.carrier, A1.state as origin, A2.state as destination
+			from vuelo V, aeropuerto A1, aeropuerto A2
+			where V.origin = A1.iata and V.destination = A2.iata
+		) T
+		group by T.carrier
+	) B
+	where A.carrier = B.carrier
+	order by porcentaje desc
 ) B
-where A.carrier = B.carrier
-order by porcentaje desc;
+where A.code = B.carrier and B.porcentaje = (
+	select MAX(porcentaje)
+	from (
+		select A.carrier, ROUND((A.num / B.num * 100), 2) as 'porcentaje'
+		from (
+			select S.carrier, COUNT(*) as num
+			from (
+				select V.carrier, A1.state as origin, A2.state as destination
+				from vuelo V, aeropuerto A1, aeropuerto A2
+				where V.origin = A1.iata and V.destination = A2.iata and A1.state = A2.state
+			) S
+			group by S.carrier
+		) A, (
+			select T.carrier, COUNT(*) as num
+			from (
+				select V.carrier, A1.state as origin, A2.state as destination
+				from vuelo V, aeropuerto A1, aeropuerto A2
+				where V.origin = A1.iata and V.destination = A2.iata
+			) T
+			group by T.carrier
+		) B
+		where A.carrier = B.carrier
+		order by porcentaje desc
+	) A
+);
 
--- vieja
-SELECT A.name FROM aerolinea A, (
-    SELECT * FROM (
-        SELECT (A.veces/T.veces), T.carrier FROM(
-            SELECT P.carrier, COUNT(*) veces FROM (
-                SELECT carrier FROM vuelo,aeropuerto A,aeropuerto B WHERE origin = A.iata 
-                AND destination = B.iata AND A.state = B.state)
-            P GROUP BY P.carrier) A,(SELECT carrier, COUNT(*) veces FROM (
-                SELECT carrier FROM vuelo) GROUP BY carrier) T
-        ORDER BY (A.veces/T.veces) DESC)
-WHERE ROWNUM = 1) B WHERE A.code = B.carrier;
